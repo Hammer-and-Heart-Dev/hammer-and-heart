0b3fb35727d25ef1558d667a358be86fca5eac97
spatel <35499137+spatelHH@users.noreply.github.com>
2025-07-01 11:17:20 -0400

    Fixes for movement
    Refine animation and fix character getting stuck on floor

--- hammer-and-heart/player.gd
+++ hammer-and-heart/player.gd
@@ -1,57 +1,72 @@
 extends CharacterBody3D
 class_name Player
 
-const SPEED = 7.5
-const JUMP_VELOCITY = 4.5
+const SPEED = 5
+const RUN_SPEED = 10.0
+const JUMP_VELOCITY = 10.0
+const ROTATION_SPEED: float = 15.0
 
 @onready var anim = $AnimationTree
-var attacking = false
+var attacking: bool = false
+var sprinting: bool
 
 
 func _input(event):
 	if attacking: return
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if event.is_action_pressed("Attack"):
 		attacking = true
-		anim["parameters/playback"].travel("Attack")
 		$Node/Skeleton3D/WeaponAttachment/Hammer/Hitbox.process_mode = Node.PROCESS_MODE_ALWAYS
-		
-	elif direction:
-		anim["parameters/playback"].travel("Walk")
-	else:
-		anim["parameters/playback"].travel("Idle")
+		anim.set("parameters/Attack/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
 
 func _ready() -> void:
+	sprinting = false
 	$Node/Skeleton3D/WeaponAttachment/Hammer/Hitbox.process_mode = Node.PROCESS_MODE_DISABLED
 	
 func _physics_process(delta: float) -> void:
+	var target_velocity = Vector3()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		target_velocity += get_gravity() * delta * 9.8
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if attacking:
-		direction = Vector3.ZERO
-	
-	if direction:
-		#anim["parameters/playback"].travel("Walk")
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	# Handle sprint
+	if Input.is_action_pressed("Sprint"):
+		sprinting = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	if direction != Vector3.ZERO:
-		direction = -direction.normalized()
-		$Node.basis = Basis.looking_at(direction)
+		sprinting = true
+	#var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
+	var input_dir := Vector3(Input.get_axis("Left", "Right"), 0, Input.get_axis("Forward", "Back")).normalized()
+
+	
+	if input_dir and not attacking:
+		#determine movement animation to use
+		if sprinting:
+			anim.set("parameters/Movement/blend_position", Vector2(0, 0.5))
+			target_velocity.x = input_dir.x * SPEED
+			target_velocity.z = input_dir.z * SPEED
+		else :
+			anim.set("parameters/Movement/blend_position", Vector2(0, 1.0))
+			target_velocity.x = input_dir.x * SPEED * 2
+			target_velocity.z = input_dir.z * SPEED * 2
+	else:
+		anim.set("parameters/Movement/blend_position", Vector2(0, 0))
+		sprinting = false
+		target_velocity.x = 0
+		target_velocity.z = 0
+		
+	velocity = target_velocity
 	move_and_slide()
+		
+	if !attacking and (Vector3(velocity.x, 0, velocity.z).length() > 0.1 or input_dir.length() > 0.1):
+		_rotate_towards($Node.position - input_dir, delta, ROTATION_SPEED)
+	
 
-
+func _rotate_towards(target: Vector3, delta: float, speed: float) -> void:
+	var target_dir = ($Node.position - target).normalized()
+	var target_rotation = atan2(target_dir.x, target_dir.z)
+	$Node.rotation.y = lerp_angle($Node.rotation.y, target_rotation, min(delta * speed, 1))
+	
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	#print(anim_name)
 	if anim_name == "Greatswordslash":
